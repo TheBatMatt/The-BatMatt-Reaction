43b39371aa16707c4ccff96bd4f30c5f2b64ee17
Ted J <tedmjablonowski@gmail.com>
2022-07-19 09:48:51 -0400

    Link deleted from home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,14 +6,11 @@ function Home() {
     <main id="homepage">
         <div className="about-para"><h1 className="sect-title">The BatMatt Reaction Portfolio: "Welcome to the Matt House!"</h1>
         </div>
-        
-            <a href="https://thebatmatt.github.io/The-BatMatt-Reaction/about" target="_blank" rel="noreferrer">
                 <img
                         src={BatHome}
                         alt="Batman"
                         className="bathome"
                         />
-            </a>
     </main>
   )
 }
